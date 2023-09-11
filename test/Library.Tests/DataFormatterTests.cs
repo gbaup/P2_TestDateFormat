9896814d76f9f8ad55398da42a00166473778140
Gaston Bauer <gaston.bauer@cencosud.com.uy>
2023-09-10 23:56:52 -0300

    primeras pruebas

---
+++ test/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,16 @@
+using TestDateFormat;
+namespace Library.Tests;
+
+public class DataFormatterTests
+{
+
+    [Test]
+    public void TestChangeFormat()
+    {
+        string date = "01/01/2021";
+        string expected = "2021-01-01";
+        string actual = DateFormatter.ChangeFormat(date);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
+
